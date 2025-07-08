3d4f7ed923b09b0297555e4c24f69ce94bbd4919
Carlos Guilherme <103211459+Guilherme-Dev-Java@users.noreply.github.com>
2025-07-08 17:49:20 -0300

    Organizando o código para ficar mais legível

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-  function toggleMenu() {
-    const navList = document.getElementById("nav-list");
-    navList.classList.toggle("active");
-  }
+function toggleMenu() {
+  const navList = document.getElementById("nav-list");
+  navList.classList.toggle("active");
+}
 
-  // Função para destacar o card clicado
-  document.addEventListener('DOMContentLoaded', () => {
-    const projetos = document.querySelectorAll('.projetos-grid article');
+// Função para destacar o card clicado
+document.addEventListener('DOMContentLoaded', () => {
+  const projetos = document.querySelectorAll('.projetos-grid article');
 
-    projetos.forEach(projeto => {
-      projeto.addEventListener('click', () => {
-        
-        projetos.forEach(p => p.classList.remove('destaque'));
+  projetos.forEach(projeto => {
+    projeto.addEventListener('click', () => {
 
-       
-        projeto.classList.add('destaque');
-      });
+      projetos.forEach(p => p.classList.remove('destaque'));
+
+
+      projeto.classList.add('destaque');
     });
   });
+});
